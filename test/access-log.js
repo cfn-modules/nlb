7807ab085beca1cadf5150ae7d87383cbd7eceb5
Michael Wittig <michael@widdix.de>
2020-08-05 15:52:21 +0200

    improve tests

--- test/access-log.js
+++ test/access-log.js
@@ -1,17 +1,6 @@
 const test = require('ava');
 const cfntest = require('@cfn-modules/test');
 
-test.serial('defaults', async t => {
-  const stackName = cfntest.stackName();
-  try {
-    t.log(await cfntest.createStack(`${__dirname}/defaults.yml`, stackName, {}));
-    // what could we test here?
-  } finally {
-    t.log(await cfntest.deleteStack(stackName));
-    t.pass();
-  }
-});
-
 test.serial('access-log', async t => {
   const stackName = cfntest.stackName();
   try {
@@ -19,9 +8,14 @@ test.serial('access-log', async t => {
     const outputs = await cfntest.getStackOutputs(stackName);
     t.log(outputs);
     // what could we test here?
-    await cfntest.emptyBucket(outputs.AccessLogBucketName);
+    t.log(await cfntest.emptyBucket(outputs.AccessLogBucketName));
   } finally {
-    t.log(await cfntest.deleteStack(stackName));
+    try {
+      t.log(await cfntest.deleteStack(stackName));
+    } catch (err) {
+      t.log(await cfntest.emptyBucket(outputs.AccessLogBucketName));
+      t.log(await cfntest.deleteStack(stackName));
+    }
     t.pass();
   }
 });
